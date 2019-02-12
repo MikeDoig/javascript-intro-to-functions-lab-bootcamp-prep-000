a80ea8f4ec52c62d4cb02eac7ead60acc6b37fdf
Mike Doig <mike.j.doig@gmail.com>
2019-02-12 02:46:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if (string.toUpperCase())
-  return ("YES INDEED!");
-  
-  else if (string.toLowerCase())
+  if (string.toLowerCase())
   return ("I can't hear you!");
   
-  else if (string === "I love you, Grandma.")
+  else if (string.toUpperCase())
+  return ("YES INDEED!");
+  
+  else if (string == "I love you, Grandma.")
   return ("I love you, too.")
   
 }
