6e23e4e3bce3f5940a17c0b170973a5e441d96a2
Mike Doig <mike.j.doig@gmail.com>
2019-02-12 02:33:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function shout(string) {
-  return string
+  return string.toUpperCase()
 }
 function whisper(string) {
-  return string
+  return string.toLowerCase()
 }
-function logShout(string){
-  return string
-}
-function logWhisper(string){
-  
-}
-function sayHiToGrandma(string){
-  
-}
+// function logShout(string){
+//   return string
+// }
+// function logWhisper(string){
+//   return string
+// }
+// function sayHiToGrandma(string){
+//   return string
+// }
