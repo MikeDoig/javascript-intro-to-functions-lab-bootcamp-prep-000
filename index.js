331085a920018debb60d20641a956f6f607c0304
Mike Doig <mike.j.doig@gmail.com>
2019-02-12 02:43:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string){
   else if (string.toUpperCase())
   return ("I can't hear you!")
   
-  else if (string = "I love you, Grandma.")
+  else if (string === "I love you, Grandma.")
   return ("I love you, too.")
   
 }
