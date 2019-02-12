f8bacca519c378e31855c3bcb38e551a49507a27
Mike Doig <mike.j.doig@gmail.com>
2019-02-12 03:35:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-  // var lowercase = "hello";
-  // var uppercase = "HELLO";
-  
+
+
 function shout(string) {
   return string.toUpperCase();
 }
@@ -14,7 +13,7 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-  // console.log(string.toUpperCase(),uppercase)
+
   if (string.toLowerCase() === string)
   return ("I can\'t hear you!");
   else if (string.toUpperCase() === string)
