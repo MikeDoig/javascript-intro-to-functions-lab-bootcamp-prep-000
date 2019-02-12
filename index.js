d4e795facd997322bea7633bccfbeb4f0cd874c1
Mike Doig <mike.j.doig@gmail.com>
2019-02-12 03:30:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
+  console.log(string.toUpperCase,uppercase)
   if (string.toLowerCase() === lowercase)
   return ("I can\'t hear you!");
   else if (string.toUpperCase() === uppercase)
