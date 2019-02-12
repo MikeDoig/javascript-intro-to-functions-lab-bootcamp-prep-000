8440ff9f884f6fd26887af7e7c19eba1645ba770
Mike Doig <mike.j.doig@gmail.com>
2019-02-12 02:56:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,28 +3,28 @@
   
 
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 function logShout(string){
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 function logWhisper(string){
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
   if (lowercase.toLowerCase() === lowercase)
   
-  return ("I can't hear you!")
+  return ("I can't hear you!");
   
   else if (uppercase.toUpperCase() === uppercase)
   
-  return ("YES INDEED!")
+  return ("YES INDEED!");
   
   else if (string == "I love you, Grandma.")
   
-  return ("I love you, too.")
+  return ("I love you, too.");
   
 }
