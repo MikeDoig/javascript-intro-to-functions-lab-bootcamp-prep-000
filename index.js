34e9f3b7a77a03091d5c9645fba72c08e76763a8
Mike Doig <mike.j.doig@gmail.com>
2019-02-12 02:32:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function shout(string) {
-  
+  return string
 }
 function whisper(string) {
-  
+  return string
 }
 function logShout(string){
-  
+  return string
 }
 function logWhisper(string){
   
